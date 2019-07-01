899cdbd69301d862d1678bb548c733f2561b243f
V-Mitch <mitchell.victorj@gmail.com>
2019-07-01 14:35:46 +0200

    canadian cpi data and surprise factor

---
+++ test.R
@@ -0,0 +1,9 @@
+# Examples for rmarkdown learning
+
+library(devtools)
+library(knitr)
+library(rmarkdown)
+library(quantmod)
+devtools::install_github("SMAC-Group/stat297")
+
+stat297::runShiny("rmd")
